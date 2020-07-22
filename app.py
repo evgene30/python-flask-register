1234e05ce4b69e3df538bb04b6580cad57556d74
EVGENE <evgene.fe@gmail.com>
2020-07-22 09:35:41 +0300

    v.1.0.12

--- app.py
+++ app.py
@@ -1,20 +1,6 @@
 from flask import Flask, render_template, url_for, request
-from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
 
 app = Flask(__name__)
-app.config['SQLALCHEMY DATABASE_URI'] = "MySQL:///users.db"
-db = SQLAlchemy(app)
-
-
-class Userreg(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    UserName = db.Column(db.String(20), nullable=False)
-    LastName = db.Column(db.String(30), nullable=False)
-    Date = db.Column(db.DateTime, default=datetime.utcnow)
-
-    def __repr__(self):
-        return '<UserReg %r>' % self.id
 
 
 @app.route('/')
@@ -30,7 +16,12 @@ def index():
 
 @app.route('/about')
 def about():
-    return render_template('about.html')
+    q = request.args.get('q')
+    if q:
+        return render_template('search.html')
+
+    else:
+        return render_template('about.html')
 
 
 @app.route('/search')
