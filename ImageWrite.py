283658bc67ea04188b37af22048c70322e47192f
EVGENE <evgene.fe@gmail.com>
2020-07-28 15:26:28 +0300

    'v.10.14.14'

---
+++ ImageWrite.py
@@ -0,0 +1,34 @@
+from PIL import Image
+
+
+def scale_image(input_image_path,
+                output_image_path,
+                width=None,
+                height=None
+                ):
+    original_image = Image.open(input_image_path)
+    w, h = original_image.size
+    print('The original image size is {wide}px wide x {height}px '
+          'high'.format(wide=w, height=h))
+
+    if width and height:
+        max_size = (width, height)
+    elif width:
+        max_size = (width, h)
+    elif height:
+        max_size = (w, height)
+    else:
+        raise RuntimeError('Width or height required!')
+
+    original_image.thumbnail(max_size, Image.ANTIALIAS)
+    original_image.save(output_image_path)
+
+    scaled_image = Image.open(output_image_path)
+    width, height = scaled_image.size
+    print('The scaled image size is {wide}px wide x {height}px '
+          'high'.format(wide=width, height=height))
+
+
+scale_image(input_image_path='1.JPG',
+            output_image_path='C:/Users/Flame/Desktop/Programming/Register/static/pillar_scaled.jpg',
+            width=900)
